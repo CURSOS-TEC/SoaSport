eef264450adbd335c0c899fd12b18ad41c31058a
Juan <jnavcamacho@gmail.com>
2018-05-19 00:07:13 -0600

    add facebook login

--- app/src/main/java/com/soa/jnavarro/soasport/MainActivity.java
+++ app/src/main/java/com/soa/jnavarro/soasport/MainActivity.java
@@ -1,7 +1,12 @@
 package com.soa.jnavarro.soasport;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+
+            Toast.makeText(MainActivity.this, user.getEmail().toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
